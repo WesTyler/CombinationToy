efe4e029b62752a6678b45aaf025e724d55bc0f8
Wes Tyler <wtyler@xogrp.com>
2016-09-30 12:48:12 -0500

    Adds my solution

--- permutations.js
+++ permutations.js
@@ -4,7 +4,33 @@ const Hoek = require('hoek');
 
 const permutations = function (requirements, exclusionSet) {
 
-    return [];
+    const overallSet = {};
+
+    const recursivePermutations = function (requirementSet, cumulativeSet) {
+
+        requirementSet.forEach((currentRequirement, index) => {
+
+            const newCumulativeSet = Hoek.clone(cumulativeSet);
+
+            if (Hoek.intersect(newCumulativeSet, exclusionSet[currentRequirement]).length === 0) {
+                newCumulativeSet.push(currentRequirement);
+
+                const stringSet = newCumulativeSet.toString();
+                overallSet[stringSet] = true;
+            }
+
+            if (requirementSet.slice(index + 1).length > 0) {
+                return recursivePermutations(requirementSet.slice(index + 1), newCumulativeSet);
+            }
+        });
+    };
+
+    recursivePermutations(requirements, []);
+
+    return Object.keys(overallSet).map((set) => {
+
+        return set.split(',');
+    });
 };
 
 module.exports = permutations;
